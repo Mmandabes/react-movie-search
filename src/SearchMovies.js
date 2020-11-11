c8cf649b3b43bf4130bdb1d6f654f87c60e73177
Mmandabes <marouanemandabes@gmail.com>
2020-11-10 19:24:16 -0500

    deploy

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -4,16 +4,22 @@ function SearchMovies() {
    
    const [query, setQuery] = useState('');
    const [movies, setMovies] = useState([]);
-
+   console.log(movies)
    const searchMovies = async (e) => {
       e.preventDefault();
-      console.log("submitting");
       const url = `https://api.themoviedb.org/3/search/movie?api_key=324c3a95be625de22fa0d97d8f4df471&language=en-US&query=${query}&page=1&include_adult=false`;
 
       try {
          const res = await fetch(url);
          const data = await res.json();
-         await setMovies(data.results)
+         const results = await data.results;
+         if ( results.length > 0){
+            await setMovies(data.results);
+            console.log(results.length)
+         } else {
+            const noResults = [{id: "0", poster_path : "/q0ZdIhd9Zb8mu3E6GFMSBoNqzYl.jpg", title: "No Results", overview : "Sorry we could not find any results" }]
+            setMovies(noResults);
+         }
       } catch (err) {
          console.log(err);
       }
@@ -33,7 +39,7 @@ function SearchMovies() {
                 {movies.filter(movie => movie.poster_path).map(movie => (
                     <div className="card" key={movie.id}>
                         <img className="card--image"
-                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${movie.poster_path}`}
                             alt={movie.title + ' poster'}
                             />
                         <div className="card--content">
