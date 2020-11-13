91c4c4772ea246b3ceb379c70a2e06a1f828ebe0
Mmandabes <marouanemandabes@gmail.com>
2020-11-13 09:42:39 -0500

    deploy

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,35 +1,10 @@
-import React, { useState } from "react";
-export default function MovieCard({ movie }) {
-   
-   function ReleaseVote(movie){
-      if (movie.release_date) {
-         return (
-            <>
-               <p><small>RELEASE DATE: {movie.release_date}</small></p>
-               <p><small>RATING: {movie.vote_average}</small></p>
-            </>
-         )
-      } else {
-         return (
-            <di>
-            </di>
-         )
-      }
-   }
-   
-   return (
-      <div className="card" key={movie.id} >
-         <img className="card--image"
-             src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-             alt={movie.title + ' poster'}
-             />
-         <div className="card--content">
-         <h3 className="card--title">{movie.title}</h3>
-         <ReleaseVote />
-         <p className="card--desc">{movie.overview}</p>
-         </div>
+import React from "react";
+import CardResult from "./CardResult";
 
-     </div>
+export default function MovieCard({ movie }) {
+
+   return (
+         <CardResult movie={movie} key={movie.id} />
    )
    
 }
